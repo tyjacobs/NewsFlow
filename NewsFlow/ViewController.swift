9639894150043099dca6958b567330cc1e166328
tyjacobs <ty.jacobs@fourthframe.com>
2015-07-27 14:24:00 -0400

    Initial Commit

---
+++ NewsFlow/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NewsFlow
+//
+//  Created by Genius on 7/27/15.
+//  Copyright (c) 2015 Simple Genius Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
